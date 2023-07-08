9840fcf7353fe13136603d84030cdf433b8c8942
Shakib Ahmed <ahmed-shakib@outlook.com>
2023-07-08 17:43:59 -0400

    showed text widgets

--- app/src/main/java/algonquin/cst2335/ahmedsandroidlabs/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/ahmedsandroidlabs/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.ahmedsandroidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +17,24 @@ public class MainActivity extends AppCompatActivity {
 
         // Loads the screen:
         setContentView(R.layout.activity_main);
-    }
 
-    // add 8 blanck spaces
+        //look for something with the id "theText", retunr the object
+        TextView theText = findViewById( R.id.theText );
+        EditText theEdit = findViewById(R.id.theEditText);
+
+        Button b = findViewById( R.id.theButton ); // will search the xml for something with id theButton
+        b.setOnClickListener(new View.OnClickListener() {
+            // Provide the missing function:
+            @Override
+            public void onClick(View v) {
+                // Reading the text from edit text and saving it to "words"
+                String words = theEdit.getText().toString(); // returns what's in the edit text
+
+                // Setting the text view text from "words"
+                theText.setText(words);
+            }
+        });
+
+
+    }
 }
